d2ad42dc96a751c4e3181d4f04d9c864c79dff9d
davodhiambo <jesseomolo@yahoo.com>
2025-04-22 20:50:25 +0300

    documentation: add comments.

--- src/rendering/mod.rs
+++ src/rendering/mod.rs
@@ -1,22 +1,24 @@
-pub mod draw;
+pub mod draw;    // Declare draw.rs as part of the 'rendering' module
+pub use draw::*; // Export all public items from draw.rs
 
 use sdl2::render::Canvas;
 use sdl2::video::Window;
 use sdl2::Sdl;
 
+// Initialize SDL2 and create a window and canvas
 pub fn init(title: &str, width: u32, height: u32) -> (Sdl, Canvas<Window>) {
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
+    let sdl_context = sdl2::init().unwrap();            // Initialize SDL2system
+    let video_subsystem = sdl_context.video().unwrap(); // Get the video subsystem
 
+    // Create a window with the specified title and dimensions
     let window = video_subsystem
         .window(title, width, height)
-        .position_centered()
-        .build()
+        .position_centered() // Center the window on the screen
+        .build()             //Builds window using builder pattern
         .unwrap();
 
+    // Create a canvas from the window
     let canvas = window.into_canvas().build().unwrap();
 
     (sdl_context, canvas)
 }
-
-pub use draw::*;
