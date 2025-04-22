73c55f474d119772694c62000da36f9488297807
davodhiambo <jesseomolo@yahoo.com>
2025-04-23 00:44:26 +0300

    feat: add vetical and horizontal lane functions. Make road darker and livelier.

--- src/rendering/draw.rs
+++ src/rendering/draw.rs
@@ -9,34 +9,43 @@ pub fn draw_roads(canvas: &mut Canvas<Window>) {
     canvas.clear();                             // Uses background color(black) to clear canvas
 
     // Set road color
-    let road_color = Color::RGB(128, 128, 128); // Gray color for the road
+    let road_color = Color::RGB(82, 94, 106); // Gray color for the road
     canvas.set_draw_color(road_color);
 
     // Draw vertical road
     let vertical_road = Rect::new(350, 0, 100, 600);
     canvas.fill_rect(vertical_road).unwrap();
 
-    // Draw top lane separator
-    let top_lane_separator = Rect::new(398, 0, 4, 250);
-    canvas.fill_rect(top_lane_separator).unwrap();
-
-     // Draw bottom lane separator
-     let bottom_lane_separator = Rect::new(398, 350, 4, 250);
-     canvas.fill_rect(bottom_lane_separator).unwrap();
-    
-    
     // Draw horizontal road
     let horizontal_road = Rect::new(0, 250, 800, 100);
     canvas.fill_rect(horizontal_road).unwrap();
 
-    // Draw left lane separator
-    let left_lane_separator = Rect::new(0, 298, 350, 4);
-    canvas.fill_rect(left_lane_separator).unwrap();
+    // Set color for lane separators
+    let lane_separator_color = Color::RGB(255, 255, 255);
+    canvas.set_draw_color(lane_separator_color);
 
-     // Draw bottom lane separator
-     let right_lane_separator = Rect::new(450, 298, 350, 4);
-     canvas.fill_rect(right_lane_separator).unwrap();
+    // Draw top lane separator
+    vertical_dotted_line(canvas, 20, 15, 398, 4, 0, 240);     // Draw top lane separator
+    vertical_dotted_line(canvas, 20, 15, 398, 4, 350, 600);   // Draw bottom lane separator 
+    horizontal_dotted_line(canvas, 4, 15, 20, 0, 350, 298);   // Draw left lane separator
+    horizontal_dotted_line(canvas, 4, 15, 20, 450, 800, 298); // Draw bottom lane separator
 
     // Present drawings to canvas
     canvas.present();
+}
+
+fn vertical_dotted_line(canvas: &mut Canvas<Window>, dash_height:u32, gap:i32, x:i32, width:u32, mut y:i32, max_y:i32) {
+    while y < max_y {
+        let dash = Rect::new(x, y, width, dash_height);
+        canvas.fill_rect(dash).unwrap();
+        y += dash_height as i32 + gap;
+    }
+}
+
+fn horizontal_dotted_line(canvas: &mut Canvas<Window>, height:u32, gap:i32, dash_width:u32, mut x:i32, max_x:i32, y:i32) {
+    while x < max_x {
+        let dash = Rect::new(x, y, dash_width, height);
+        canvas.fill_rect(dash).unwrap();
+        x += dash_width as i32 + gap;
+    }
 }
