249a754d8766d89bb6cb63e7075445c7f5ac27b3
davodhiambo <jesseomolo@yahoo.com>
2025-04-22 19:45:11 +0300

    add logic in main.rs to render intersection.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+mod rendering;
+
+use rendering::{draw, init};
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let (sdl_context, mut canvas) = init::init("Traffic Simulator", 800, 600);
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            use sdl2::event::Event;
+            match event {
+                Event::Quit { .. } => break 'running,
+                _ => {}
+            }
+        }
+
+        draw::draw_roads(&mut canvas);
+        std::thread::sleep(Duration::from_millis(16)); // ~60 FPS
+    }
 }
