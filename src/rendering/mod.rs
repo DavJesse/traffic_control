6c321d86a167cdf648bad4038f7303477c9fad2c
davodhiambo <jesseomolo@yahoo.com>
2025-04-22 19:28:14 +0300

    feat: add init function to create SDL2 window and canvas.

---
+++ src/rendering/mod.rs
@@ -0,0 +1,18 @@
+use sdl2::render::Canvas;
+use sdl2::video::Window;
+use sdl2::Sdl;
+
+pub fn init(title: &str, width: u32, height: u32) -> (Sdl, Canvas<Window>) {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem
+        .window(title, width, height)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let canvas = window.into_canvas().build().unwrap();
+
+    (sdl_context, canvas)
+}
